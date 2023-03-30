e579a453a2647ba654486cad06307a541e205ffe
suryogumilar <suryo.gumilar@gmail.com>
2023-03-30 05:05:34 -0700

    bugfix for run_wildfly_config=true, not yet solved, but ok for run_wildfly_config=false

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Applications used in this image are:
  - Almalinux 9.1
  - Wildfly version 26.1.2-Final   
    This is the default version, we can change it runtimely during build using 
-   `--build-arg` e.g: `docker build --build-arg WILDFLY_INSTALL_VERSION=26.1.2-Final`
+   `--build-arg` e.g: `docker build --build-arg WILDFLY_VERSION=26.1.2-Final`
  - JDK java-11-openjdk-devel.x86_64 (java-11-openjdk-11.0.18.0.10-2.el8_7.x86_64)
  - Ant apache-ant-1.10.13
  - Signserver CE 5.11.1
@@ -18,3 +18,47 @@ This image is also prepared for HSM connection using HSM utimaco either *Securit
 docker build -t <tag_name>:<tag_version> -f Dockerbuild.ss511 .
 
 the <tag_name>:<tag_version> examples is: local_signserver:5.11
+
+full example : 
+
+```sh
+docker build --build-arg WILDFLY_VERSION=26.1.3.Final --build-arg WAIT_FOR_WILDFLY=10 --build-arg DISABLE_MANAGEMENT_WEB_CONSOLE=true --build-arg REMOVE_WILDFLY_WELCOME_CONTENT=true -t local_signserver:5.11.1-Final -f Dockerbuild.ss511 .
+
+```
+
+### build-args
+
+build argument and also envi variables for running a container
+
+ - WILDFLY_VERSION="26.1.3.Final"
+ - WILDFLY_REPO_URL=https://github.com/wildfly/wildfly/releases/download
+ - ENABLE_GLOWROOT=false
+ - RUN_WILDFLY_CONFIG=false
+ - DISABLE_MANAGEMENT_WEB_CONSOLE=false
+ - REMOVE_WILDFLY_WELCOME_CONTENT=false
+ - SIGNSERVER_VERSION=signserver-ce-5.11.1.Final
+ - KEYSTORE_PASSWORD=foo123
+ - TRUSTSTORE_PASSWORD=foobar
+ - DB_PASSWORD=passw0rd
+ - WAIT_FOR_WILDFLY=13
+
+##### Problem on using `RUN_WILDFLY_CONFIG=true`
+
+for `RUN_WILDFLY_CONFIG` if set to true and container restarted it would mess the signserver since it already deployed. It also conflicted with `REMOVE_WILDFLY_WELCOME_CONTENT` if set to `true`
+
+### Running the container
+
+
+```sh
+docker run -it --name lss_2612 \
+ -e WAIT_FOR_WILDFLY_TO_RUN=10 \
+ -e RUN_WILDFLY_CONFIG=false \
+ -e ENABLE_GLOWROOT=false \
+ -p 8888:8080 -p 8443:8443 -p 8442:8442 -p 9990:9990 \
+ -p 4000:4000 \
+ -v ./certificates_dir/certa_wildfly/wildfly_keystore.p12:/opt/wildfly/standalone/configuration/keystore/wildfly_keystore.p12:ro \
+ -v ./certificates_dir/certa_client_wildfly/apptrustore.jks:/opt/wildfly/standalone/configuration/keystore/truststore.jks \
+ -v ./transit_folder:/mnt/transit_folder \
+ --network=wfNetwork \
+ local_signserver:5.11.1-Final
+```
--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,78 @@
 #/bin/bash
 
-/run_wildfly.sh &
+if [ ! "$RUN_WILDFLY_CONFIG" = true ]; then
+  source /etc/profile.d/wildfly_setenv.sh
+  
+  if [ ! -f /opt/wildfly/standalone/deployments/mariadb-java-client.jar ]; then
+    # deploy mariadb client jar
+
+    wget https://dlm.mariadb.com/1785291/Connectors/java/connector-java-2.7.4/mariadb-java-client-2.7.4.jar\
+     -O /opt/wildfly/standalone/deployments/mariadb-java-client.jar
+  fi
+
+  # create default keystore and keystore entry
+  PATH_CONFIG_KEYSTORE=/opt/wildfly/standalone/configuration/keystore
+
+  if [ ! -d "$PATH_CONFIG_KEYSTORE" ]; then
+    echo "$PATH_CONFIG_KEYSTORE does not exist."
+    mkdir -p /opt/wildfly/standalone/configuration/keystore
+    chown wildfly:wildfly $PATH_CONFIG_KEYSTORE
+  fi
+
+  if [ ! -f /usr/bin/wildfly_pass ]; then
+
+    # Create an Elytron Credential Store
+    echo '#!/bin/sh' > /usr/bin/wildfly_pass
+    echo "echo '$(openssl rand -base64 24)'" >> /usr/bin/wildfly_pass
+    chown wildfly:wildfly /usr/bin/wildfly_pass
+    chmod 700 /usr/bin/wildfly_pass
+  fi
+  
+  if [ ! -f /opt/wildfly/standalone/configuration/keystore/credentials ]; then
+    
+    ## create credential store
+    echo "create new credential store"
+    elytron-tool.sh credential-store --create \
+     --location /opt/wildfly/standalone/configuration/keystore/credentials \
+     --password `/usr/bin/wildfly_pass`
+
+    ## add alias entry -change this via envi variables
+    elytron-tool.sh credential-store \
+     --location /opt/wildfly/standalone/configuration/keystore/credentials \
+     --password `/usr/bin/wildfly_pass` \
+     --add httpsKeystorePassword --secret "$KEYSTORE_PASSWORD"
+
+    elytron-tool.sh credential-store \
+     --location /opt/wildfly/standalone/configuration/keystore/credentials \
+     --password `/usr/bin/wildfly_pass` \
+     --add httpsTruststorePassword --secret "$TRUSTSTORE_PASSWORD"
+  
+    elytron-tool.sh credential-store \
+     --location /opt/wildfly/standalone/configuration/keystore/credentials \
+     --password `/usr/bin/wildfly_pass` \
+     --add dbPassword --secret "$DB_PASSWORD"
+  fi
+
+  unset DB_PASSWORD
+  unset TRUSTSTORE_PASSWORD
+  unset KEYSTORE_PASSWORD 
+
+fi
 
 if [ "$RUN_WILDFLY_CONFIG" = true ]; then
-  sleep $WAIT_FOR_WILDFLY_TO_RUN
+  echo "reset standalone.xml config"
+  cp /opt/wildfly/standalone/configuration/standalone_init0.xml \
+     /opt/wildfly/standalone/configuration/standalone.xml
+fi
+
+## run wildfly
+/run_wildfly.sh &
+
+## run config if set
+if [ "$RUN_WILDFLY_CONFIG" = true ]; then
+  sleep $WAIT_FOR_WILDFLY
+  echo "undeploy signserver first"
+  /opt/wildfly/bin/jboss-cli.sh --connect 'undeploy signserver.ear'
   ## run config scripts
   echo "config wildfly remoting"
   bash /usr/local/wildfly/config_script/config_wildfly_remoting.sh
@@ -17,29 +86,49 @@ if [ "$RUN_WILDFLY_CONFIG" = true ]; then
   bash /usr/local/wildfly/config_script/config_widlfy_remove_exampleDS.sh
   echo "config datasource mariadb"
   bash /usr/local/wildfly/config_script/config_wildfly_datasource_mariadb_4signserver.sh
+else
+  echo "waiting wildfly to up"
+  sleep $WAIT_FOR_WILDFLY
 fi
 
 if [ "$ENABLE_GLOWROOT" = true ]; then
   bash /usr/local/wildfly/config_script/install_glowroot.sh
   echo "glowroot installed!"
+  export ENABLE_GLOWROOT=false
 fi
 
-if [ "$DISABLE_MGMT_WEB_CONSOLE" = true ]; then
+if [ "$DISABLE_MANAGEMENT_WEB_CONSOLE" = true ]; then
   bash /usr/local/wildfly/config_script/config_wildfly_disable_mgmt_web_console.sh
   echo "management console disabled!"
+  export DISABLE_MANAGEMENT_WEB_CONSOLE=false
 fi
 
-if [ "$REMOVE_WF_WELCOME_CONTENT" = true ]; then
+if [[ ( "$REMOVE_WILDFLY_WELCOME_CONTENT" = true ) && ( -d /opt/wildfly/welcome-content ) ]]; then
   bash /usr/local/wildfly/config_script/config_wildfly_remove_welcome_content.sh
   echo "wildfly welcome content removed!"
+  export REMOVE_WILDFLY_WELCOME_CONTENT=false
 fi
-
 
 ## set signserver envi
 source /etc/profile.d/envi_signserver.sh
 
-# deploy
-cd /opt/$SIGNSERVER_VERSION
-ant deploy
+if [[ (! -f /opt/wildfly/standalone/deployments/signserver.ear.deployed) || ("$RUN_WILDFLY_CONFIG" = true) ]]; then
+  # deploy
+  cd /opt/$SIGNSERVER_VERSION
+  ant deploy
+  echo "signserver deployed"
+else
+  echo "no deployment action taken since signserver already deployed"
+fi
+
+## cleaning up
+
+if [ ! -f /etc/profile.d/unset_pass.sh ]; then
+  echo "create unset_pass.sh"
+  echo "#!/bin/bash" > /etc/profile.d/unset_pass.sh
+  echo "unset DB_PASSWORD" >> /etc/profile.d/unset_pass.sh
+  echo "unset TRUSTSTORE_PASSWORD" >> /etc/profile.d/unset_pass.sh
+  echo "unset KEYSTORE_PASSWORD" >> /etc/profile.d/unset_pass.sh
+fi
 
 sleep infinity
