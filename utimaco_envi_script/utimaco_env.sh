69260098c31e65c8104a05b7fdb44fd66ecb6080
suryogumilar <suryo.gumilar@gmail.com>
2023-04-06 05:05:30 -0700

    add envi for utimaco

---
+++ utimaco_envi_script/utimaco_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CS_PKCS11_R2_CFG=/etc/utimaco/cs_pkcs11_R2.cfg
+export CS_PKCS11_R2_CFG
+
+CS_PKCS11_R3_CFG=/etc/utimaco/cs_pkcs11_R3.cfg
+export CS_PKCS11_R3_CFG
+
+CRYPTO_SERVER_IP=hsm_simulator
+export CRYPTO_SERVER_IP
